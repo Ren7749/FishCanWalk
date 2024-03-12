abd63866d7a6f44bafe19a725ae155cfa0b5618e
Ren7749 <159054824+Ren7749@users.noreply.github.com>
2024-03-13 02:59:28 +0700

    TopDown

---
+++ TopDown/Assets/Scrip/RespawnPlayer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPlayer : MonoBehaviour
+{
+    //[SerializeField]
+    //private Transform player;
+    //[SerializeField]
+    //private Transform respawn_point;
+
+    public PlayerControl playerControl;
+
+    public bool OnHit;
+
+    //public GameObject playerPoint;
+
+    public GameObject spawnPoint;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Obtacle"))
+        {
+            OnHit = true;
+            Respawn();
+            playerControl.enabled = false;
+        }
+    }
+
+    public void Respawn()
+    {
+        transform.position = spawnPoint.transform.position;
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.CompareTag("Obtacle"))
+        {
+            OnHit = false;
+            playerControl.enabled = true;
+        }
+    }
+}
